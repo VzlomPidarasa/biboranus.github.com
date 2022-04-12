7a8587f64e2ca851440098f585ae437940ec85e2
VzlomPidarasa <vladvas16062012@gmail.com>
2022-04-12 13:25:30 +0300

    sfd
    fdf

---
+++ TitanMiner.py
@@ -0,0 +1,11 @@
+import time
+from random import randint
+a = 1
+
+if a == 1:
+    print("TitanMiner activated, wait 5 second")
+time.sleep(5)
+
+while a == 1:
+    print("+1 TitanCoin", randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9) )
+    time.sleep(3) 
